3430fd16e45696fb545fe32e214b045adbb75dca
mzj <943885179@qq.com>
2017-09-14 16:35:42 +0800

    Merge branch 'master' of /Users/ios13/Desktop/adroiditem/account with conflicts.

--- app/src/main/java/chen/com/myaccount/Home.java
+++ app/src/main/java/chen/com/myaccount/Home.java
@@ -1,19 +1,22 @@
 package chen.com.myaccount;
 
-import android.app.Fragment;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public  class Home extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener,
-        ViewPager.OnPageChangeListener,OutComeFragment.OnFragmentInteractionListener{
+import java.lang.reflect.Field;
+
+public class Home extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener,
+        ViewPager.OnPageChangeListener {
     private TextView txt_topbar;
     private RadioGroup rg_tab_bar;
     private RadioButton rb_channel;
@@ -24,34 +27,35 @@ public  class Home extends AppCompatActivity implements RadioGroup.OnCheckedChan
 
     private MyFagementPagerAdapter mAdapter;
 
-    public static final int page_one=0;
-    public static final int page_two=1;
-    public static final int page_three=2;
-    public static final int page_four=3;
+    public static final int page_one = 0;
+    public static final int page_two = 1;
+    public static final int page_three = 2;
+    public static final int page_four = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        mAdapter=new MyFagementPagerAdapter(getSupportFragmentManager());
+        mAdapter = new MyFagementPagerAdapter(getSupportFragmentManager());
         bindViews();
         rb_channel.setChecked(true);
     }
 
-    private void bindViews(){
-        txt_topbar=(TextView)findViewById(R.id.txt_context);
-        rg_tab_bar=(RadioGroup)findViewById(R.id.rg_tab_bar);
-        rb_channel=(RadioButton)findViewById(R.id.rb_channel);
-        rb_message=(RadioButton)findViewById(R.id.rb_message);
-        rb_better=(RadioButton)findViewById(R.id.rb_better);
-        rb_setting=(RadioButton)findViewById(R.id.rb_setting);
+    private void bindViews() {
+        txt_topbar = (TextView) findViewById(R.id.txt_context);
+        rg_tab_bar = (RadioGroup) findViewById(R.id.rg_tab_bar);
+        rb_channel = (RadioButton) findViewById(R.id.rb_channel);
+        rb_message = (RadioButton) findViewById(R.id.rb_message);
+        rb_better = (RadioButton) findViewById(R.id.rb_better);
+        rb_setting = (RadioButton) findViewById(R.id.rb_setting);
         rg_tab_bar.setOnCheckedChangeListener(this);
 
-        vpager=(ViewPager)findViewById(R.id.vpager);
+        vpager = (ViewPager) findViewById(R.id.vpager);
         vpager.setAdapter(mAdapter);
         vpager.setCurrentItem(0);
         vpager.addOnPageChangeListener(this);
     }
+
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -64,27 +68,27 @@ public  class Home extends AppCompatActivity implements RadioGroup.OnCheckedChan
 
     @Override
     public void onPageScrollStateChanged(int state) {
-    if (state==2){
-        switch ((vpager.getCurrentItem())){
-            case page_one:
-                rb_channel.setChecked(true);
-                break;
-            case page_two:
-                rb_message.setChecked(true);
-                break;
-            case page_three:
-                rb_better.setChecked(true);
-                break;
-            case page_four:
-                rb_setting.setChecked(true);
-                break;
+        if (state == 2) {
+            switch ((vpager.getCurrentItem())) {
+                case page_one:
+                    rb_channel.setChecked(true);
+                    break;
+                case page_two:
+                    rb_message.setChecked(true);
+                    break;
+                case page_three:
+                    rb_better.setChecked(true);
+                    break;
+                case page_four:
+                    rb_setting.setChecked(true);
+                    break;
+            }
         }
-    }
     }
 
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-        switch (i){
+        switch (i) {
             case R.id.rb_channel:
                 vpager.setCurrentItem(page_one);
                 break;
@@ -99,8 +103,31 @@ public  class Home extends AppCompatActivity implements RadioGroup.OnCheckedChan
                 break;
         }
     }
-    private FragmentManager fm = null;
+
+    //右上角图标－－start//
     @Override
-    public void onFragmentInteraction(Uri uri) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.item1:
+                Toast.makeText(Home.this, "添加收入", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(Home.this, "添加支出", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(Home.this, "添加便签", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //右上角菜单－－end//
 }
